defa035e7c67cc7630bffb23bcda3dab3ef2abb8
samiaul <samiaul.56@gmail.com>
2026-01-06 11:47:35 +0100

    feat(Clock): improved query params for user/{id}/clocks

---
+++ backend/src/main/java/eu/epitech/t_dev_700/models/UserScheduleQuery.java
@@ -0,0 +1,34 @@
+package eu.epitech.t_dev_700.models;
+
+import eu.epitech.t_dev_700.models.constraints.ValidScheduleQuery;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+@Setter
+@Getter
+@ValidScheduleQuery
+@Schema(description = "Query parameters for filtering schedules")
+public class UserScheduleQuery {
+
+    @Schema(
+            description = "Start date (ISO-8601). " + "Cannot be used with `current`.",
+            example = "2024-01-15T10:30:00Z"
+    )
+    private OffsetDateTime from;
+
+    @Schema(
+            description = "End date (ISO-8601). " + "Cannot be used with `current`.",
+            example = "2024-01-15T10:30:00Z"
+    )
+    private OffsetDateTime to;
+
+    @Schema(
+            description = "If true, returns only current schedules. " + "Cannot be combined with `from` or `to`.",
+            defaultValue = "false",
+            example = "true"
+    )
+    private Boolean current = false;
+}
